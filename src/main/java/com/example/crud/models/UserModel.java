6ddf3fdb73bc8c3c3007fe85013dbaa7a3406008
Pedro Alves de Amorim Neto <piromaticos@gmail.com>
2023-02-10 16:03:59 -0300

    tirando status ativo

--- src/main/java/com/example/crud/models/UserModel.java
+++ src/main/java/com/example/crud/models/UserModel.java
@@ -22,9 +22,6 @@ public class UserModel {
     @JsonIgnore
     private String password;
 
-    @Column(nullable = false)
-    private Boolean ativo;
-
     public Long getId() {
         return id;
     }
@@ -49,13 +46,44 @@ public class UserModel {
         this.password = password;
     }
 
-    public Boolean getAtivo() {
-        return ativo;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        return result;
     }
 
-    public void setAtivo(Boolean ativo) {
-        this.ativo = ativo;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserModel other = (UserModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        return true;
     }
+
+
 
 
 }
